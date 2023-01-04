ec7fee8d4b85a06b302d1d1811197056fcb2f8db
nan-mu <mu.nan@88.com>
2023-01-04 23:44:49 +0800

    写洗牌的实验报告～

--- main.cpp
+++ main.cpp
@@ -18,8 +18,8 @@ int main() {
 
     roundManager p21;  // 初始化回合管理员
     //  交付环境下不使用debug，但在此处使用Player提供的接口编写ai逻辑，并在此处将玩家
-    //  p21.debug();  // debug内加载了用于测试的玩家对象
-    p21.pvp(2, false);  // 交付环境使用
+    p21.debug();  // debug内加载了用于测试的玩家对象
+    // p21.pvp(2, false);  // 交付环境使用
 
     return printf("\n");
 }
