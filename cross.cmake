a69375d959b4c74c713f28c38c196301ad0685a0
nan-mu <mu.nan@88.com>
2023-01-14 16:39:00 +0800

    跨平台还是失败了... 下一个思路是弃用cmake 我自己写ninja

--- cross.cmake
+++ cross.cmake
@@ -1,4 +1,6 @@
 set(CMAKE_SYSTEM_NAME windows)
 
 # ​set(CMAKE_C_COMPILER /usr/bin/x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++-posix)
+
+# x86_64-w64-mingw32-c++-win32
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
